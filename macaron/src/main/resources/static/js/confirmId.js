d0905d16a037822cc4eceda39ce8fc13d48f7d35
kkonii <119216472+kkonii@users.noreply.github.com>
2023-07-12 21:21:06 +0900

    원경연 - 회원가입 중복체크 안됨 . .

---
+++ macaron/src/main/resources/static/js/confirmId.js
@@ -0,0 +1,117 @@
+$(function(){
+	let checkId = 0;
+	
+	//=============아이디 중복 체크=============
+	$('#confirmId').click(function(){
+		if($('#mem_id').val().trim()==''){
+			$('#message_id').css('color','#E65962').text('아이디를 입력하세요');
+			$('#mem_id').val('').focus();
+			return;
+		}
+	
+		//서버와 통신
+		$.ajax({
+			url:'confirmId.do',
+			type:'post',
+			data:{mem_id:$('#mem_id').val()},
+			dataType:'json',
+			success:function(param){
+				if(param.result == 'idNotFound'){
+					$('#message_id').css('color','#000').text('사용가능합니다.');
+					checkId = 1;	
+				}else if(param.result == 'idDuplicated'){
+					$('#message_id').css('color','#E65962').text('존재하는 아이디입니다.');
+					checkId = 0;
+				}else if(param.result == 'notMatchPattern'){
+					$('#message_id').css('color','#E65962').text('4~12자의 영문 소문자, 숫자만 사용 가능합니다.');
+					checkId = 0;
+				}else{
+					checkId = 0;
+					alert('아이디 중복체크 오류');
+				}
+			},
+			error:function(){
+				checkId = 0;
+				alert('네트워크 오류 발생');
+			}
+		});	
+		
+	}); //end of click
+	
+	//=========아이디 중복 안내 메시지 초기화 및 아이디 중복 값 초기화=========
+	$('#register_form #mem_id').keydown(function(){
+		checkId = 0;
+		$('#message_id').text('');
+	}); //end of keydown
+	
+	//submit 이벤트 발생시 아이디 중복 체크 여부 확인
+	$('#register_form').submit(function(){
+		if(checkId==0){
+			$('#message_id').css('color','#E65962').text('중복체크 필수');
+			if($('#mem_id').val().trim()==''){
+				$('#mem_id').val('').focus();
+			}
+			return false;
+		}
+	}); //end of submit
+	
+	
+	//=============비밀번호 확인===============
+	$("#mem_pw,#pw_confirm").keyup(function(){
+		let mem_pw = $("#mem_pw").val();
+		let pw_confirm = $("#pw_confirm").val();
+		
+		if(mem_pw=='' && pw_confirm==''){
+			$("#confirmMsg").text("");
+			return;
+		}
+		
+		if(mem_pw!='' && pw_confirm!=''){
+			if(mem_pw == pw_confirm){
+				$("#confirmMsg").text("비밀번호 일치").css('color','#000');
+				return;
+			}
+			if(mem_pw != pw_confirm){
+				$("#confirmMsg").text("비밀번호 불일치").css('color','#E65962');
+				return;
+			}
+		}
+	});
+	
+	
+	
+	$('#register_form').submit(function(){
+		//공백입력시
+		if($('#mem_id').val().trim()==''){
+			$("#login_Id").text("아이디를 입력하세요");
+			$('#mem_id').val('').focus();
+			return false;
+		}
+		if($('#mem_pw').val().trim()==''){
+			$("#login_Pw").text("비밀번호를 입력하세요");
+			$('#mem_pw').val('').focus();
+			return false;
+		}
+		//유효성
+		let right_id = /^[a-z0-9]{4,12}$/;
+		let right_pw = /^[A-Za-z0-9]{8,16}$/;
+
+    	if(!right_id.test(mem_id.value)){
+      		$('#login_Id').text("4~12자의 영문 소문자, 숫자로 입력하세요.");
+			$('#mem_id').val('').focus();
+			return false;
+		}
+		
+		if(!right_pw.test(mem_pw.value)){
+      		$('#login_Pw').text("8~16자의 영문 대소문자, 숫자로 입력하세요.");
+			$('#mem_pw').val('').focus();
+			return false;
+		}
+    	
+	});
+	
+	
+
+});
+
+
---
+++ macaron/src/main/resources/static/js/makeEmail.js
@@ -0,0 +1,138 @@
+$(function(){
+	
+	//=================이메일 합치기==================
+	$("#email_direct").hide();
+	
+	//이메일 앞부분
+	$("#user_email").change(function(){
+		email();
+	});
+
+	$("#email_address").change(function() {
+	if($("#email_address").val() == "direct"){
+		$("#email_direct").show();
+		$(document).on('keyup','#email_direct',function(){
+			email();
+		});
+	}else{
+		$("#email_direct").hide();
+		email();
+	}
+	});
+	
+	
+	//합치기
+	function email(){
+		let email = $("#user_email").val();
+		let middle = $("#middle").text();
+		var address;
+		if($("#email_address").val()=="direct"){
+			address = $("#email_direct").val();
+		}else{
+			address = $("#email_address").val();
+		}
+		
+		if(email!='' && address!=''){
+			$("#mem_email").val(email+middle+address);
+		}
+	};
+	
+	
+	//============이메일 인증코드==============
+	$('#mail-Check-Btn').click(function() {
+		if($('#user_email').val()==''){
+			$('#user_email').val('').focus();
+			$('#mail-check-warn').html('이메일을 입력해주세요');
+			$('#mail-check-warn').css('color','#E65962');
+			return;
+		}else{
+			$('#mail-check-warn').html('');
+			
+		if($("#email_address").val()=="direct"){
+			if($('#email_direct').val()==''){
+				$("#email_direct").show();
+				$('#email_direct').val('').focus();
+				$('#mail-check-warn').html('이메일을 입력해주세요');
+				$('#mail-check-warn').css('color','#E65962');
+				return;
+			}
+		}
+			
+			const email = $('#mem_email').val();
+			console.log('완성된 이메일 : ' + email); // 이메일 오는지 확인
+			const checkInput = $('.mail-check-input') // 인증번호 입력하는곳 
+			
+			$.ajax({
+				type : 'get',
+				url : '../mailCheck?email='+email, // GET방식이라 Url 뒤에 email 붙임
+				success : function (data) {
+					checkInput.attr('disabled',false);
+					code =data;
+					alert('인증번호가 전송되었습니다.');
+					console.log(code);
+				}			
+			});
+		}
+				
+	}); // end send eamil
+
+	// 인증번호 비교 
+	$('.mail-check-input').keyup(function () {
+		const inputCode = $(this).val();
+		const $resultMsg = $('#mail-check-warn');
+		
+		if(inputCode === code){
+			$resultMsg.html('인증번호가 일치합니다');
+			$resultMsg.css('color','green');
+		}else{
+			$resultMsg.html('인증번호가 불일치 합니다');
+			$resultMsg.css('color','#E65962');
+			
+		}
+	});
+
+
+	$('#mem_pw').keyup(function() {
+		let right_pw = /^[A-Za-z0-9]{8,16}$/;
+		if(!right_pw.test(mem_pw.value)){
+     	 	$('#message_pw').text("8~16자의 영문 대소문자, 숫자로 입력하세요.");
+			$('#message_pw').css('color','#E65962');
+			return;
+		}else{
+			$('#message_pw').text('');
+		}
+	
+	});
+
+	//유효성 체크
+	$('#register_form').submit(function(){
+		
+		if($("#email_address").val()=="direct"){
+			if($('#email_direct').val()==''){
+				$("#email_direct").show();
+				$('#email_direct').val('').focus();
+				return false;
+			}
+		}
+		
+		if($('.mail-check-input').val()==''){
+			$('.mail-check-input').val('');
+			$('#mail-check-warn').html('인증번호를 입력해주세요');
+			$('#mail-check-warn').css('color','#E65962');
+			return false;
+		}
+		
+		if($('.mail-check-input').val()!==code){
+			const $resultMsg = $('#mail-check-warn');
+			$resultMsg.html('인증번호가 불일치 합니다');
+			$resultMsg.css('color','#E65962');
+			return false;
+		}
+		
+	});
+	
+		
+});
+
+	
+
