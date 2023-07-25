09a4a079f12c35e9037ab294662ffaa163acf34f
kkonii <119216472+kkonii@users.noreply.github.com>
2023-07-25 20:25:01 +0900

    원경연 - 캘린더 일정 등록 controller mapping

--- macaron/src/main/java/kr/spring/calendar/controller/CalendarController.java
+++ macaron/src/main/java/kr/spring/calendar/controller/CalendarController.java
@@ -2,15 +2,35 @@ package kr.spring.calendar.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.spring.calendar.service.CalendarService;
+import kr.spring.calendar.vo.CalendarVO;
 
 @Controller
 public class CalendarController {
 	private static final Logger logger = LoggerFactory.getLogger(CalendarController.class);
 	
+	@Autowired
+	private CalendarService calendarService;
+	
+	@ModelAttribute
+	public CalendarVO initCommand() {
+		return new CalendarVO();
+	}
+	
 	@RequestMapping("/cal/main.do")
 	public String main() {
 		return "calMain";
+	}
+	
+
+	@GetMapping("/cal/write.do")
+	public String galleryWriteForm() {
+		return "calWrite";
 	}
 }
